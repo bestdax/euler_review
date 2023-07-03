369d516192f24d2613f5b99e37ecea0d1e4ae3c7
dax <dax26d@gmail.com>
2023-07-03 17:26:43 +0800

    improved solution for problem 09

--- src/problem09/problem.cpp
+++ src/problem09/problem.cpp
@@ -9,7 +9,7 @@ dax 2023-07-01 15:39:11
 void Solution::answer()
 {
 	for(unsigned a = 1; a < 333; ++a)
-		for(unsigned b = a + 1; 1000 - a - b > b; ++b)
-			if(a * a + b * b == (1000 - a - b) * (1000 - a - b))
-				std::cout << "The answer is: " << a * b * (1000 - a - b) << std::endl;
+		for(unsigned b = a + 1, c = 1000 - a - b; 1000 - a - b > b; ++b, c = 1000 - a - b)
+			if(a * a + b * b == c * c)
+				std::cout << "The answer is: " << a * b * c << std::endl;
 }
